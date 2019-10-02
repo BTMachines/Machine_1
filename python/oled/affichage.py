014c1dae26c6a5808b0bc9eb79f89c77f022a1e9
totolabricot <thomas-barbe@hotmail.com>
2019-10-03 00:21:01 +0200

    premier_jet

---
+++ python/oled/affichage.py
@@ -0,0 +1,137 @@
+
+import time
+
+import Adafruit_GPIO.SPI as SPI
+import Adafruit_SSD1306
+
+from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageFont
+
+import subprocess
+
+# Raspberry Pi pin configuration:
+RST = None     # on the PiOLED this pin isnt used
+# Note the following are only used with SPI:
+DC = 23
+SPI_PORT = 0
+SPI_DEVICE = 0
+
+disp = Adafruit_SSD1306.SSD1306_128_64(rst=RST)
+
+
+disp.begin()
+disp.clear()
+disp.display()
+
+last=[0,0,0]
+
+
+width = disp.width
+height = disp.height
+image = Image.new('1', (width, height))
+carre_width=width/18
+carre_size=4
+carre2_width=width/4
+carre2_size=8
+marge_top=20
+carre_height=(height-marge_top)/4
+carre2_height=(height-marge_top)/3
+
+lastpas=0
+
+def affMenu(idInstru,listMute):
+    draw = ImageDraw.Draw(image)
+    draw.rectangle((0,0,width,height), outline=0, fill=0)
+    font = ImageFont.load_default()
+    draw.text((0,0),"track",  font=font, fill=255)
+    draw.text((40,0),str(idInstru),  font=font, fill=255)
+    if listMute[idInstru-1]==True:
+        draw.text((70,0),"mute",  font=font, fill=255)
+    
+    j=0
+    i=0
+    idCount=0
+    while j<3:
+        while i<4: 
+            draw.text((i*carre2_width,marge_top+j*carre2_height),str(idCount+1),  font=font, fill=255)
+
+            if idCount==idInstru-1:
+                draw.rectangle((i*carre2_width+15,marge_top+j*carre2_height,i*carre2_width+12,marge_top+j*carre2_height+carre2_size), outline=255, fill=255) #center 
+
+            if listMute[idCount]==False:
+                draw.rectangle((i*carre2_width+15,marge_top+j*carre2_height,i*carre2_width+carre2_size+15,marge_top+j*carre2_height+carre2_size), outline=255, fill=0) #center 
+            else:  
+                draw.rectangle((i*carre2_width+18,marge_top+j*carre2_height+3,i*carre2_width+carre2_size+12,marge_top+j*carre2_height+carre2_size-3), outline=255, fill=255) #center 
+            i+=1
+            idCount+=1
+        j+=1
+        i=0
+    disp.image(image)
+    disp.display()
+
+def affSeq(idInstru,idPas,pas,listVelo):
+
+
+    draw = ImageDraw.Draw(image)
+    draw.rectangle((0,0,width,height), outline=0, fill=0)
+    font = ImageFont.load_default()
+    draw.text((0,0),str(round(idInstru)),  font=font, fill=255)
+    draw.text((30,0),str(round(idPas)),  font=font, fill=255)
+    draw.text((60,0),str(listVelo[idPas]),  font=font, fill=255)
+    #draw.text((20,0),str(round(idPas)),  font=font, fill=255)
+    idPas=round(idPas);
+    #print("idpas : ",idPas)
+    i=0
+    per_line=16
+    pas_pos=[0,0,0]
+    reste=idPas
+    j=0
+    i=0
+    pas=pas*4
+    
+    
+    
+    while j<3:
+        while i<4:
+            if reste>=i*per_line:
+                pas_pos[j]=i;
+            i+=1
+        #print("pos",j,":",pas_pos[j])
+        i=0
+        j+=1
+        reste=reste-pas_pos[j-1]*per_line
+        #print("reste",j,": ",reste)
+        per_line=per_line/4
+        
+    j=0
+    idCount=0
+    col=0
+    xInc=0
+    margeInterval=4
+    
+    while j<4:
+        while i<16: 
+            if listVelo[idCount]!=0:
+                col=255
+            else:
+                col=0
+            draw.rectangle((i*carre_width+xInc,marge_top+j*carre_height,i*carre_width+carre_size+xInc,marge_top+j*carre_height+carre_size), outline=255, fill=col) #center 
+            if pas==idCount:
+                draw.rectangle((i*carre_width+xInc,marge_top+j*carre_height+carre_size,i*carre_width+carre_size+xInc,marge_top+j*carre_height+carre_size+1), outline=255, fill=255) #center 
+
+            
+            idCount+=1
+            i+=1
+            if i%4==0:
+                xInc+=margeInterval
+        j+=1
+        i=0
+        xInc=0
+
+    pos_1=pas_pos[1]*carre_width*4+pas_pos[2]*carre_width+pas_pos[1]*margeInterval
+    draw.rectangle((pos_1,marge_top+pas_pos[0]*carre_height,pos_1+carre_size,marge_top+pas_pos[0]*carre_height-3), outline=255, fill=255) #center 
+    
+    disp.image(image)
+    disp.display()
+    
---
+++ python/osc_blocking_server.py
@@ -0,0 +1,83 @@
+from pythonosc.dispatcher import Dispatcher
+from pythonosc.osc_server import BlockingOSCUDPServer
+from oled.affichage import *
+
+lastIdInstru=1
+lastIdPas=0
+lastPas=0
+lastIdMenu=0
+
+list_velos=[[],[],[],[]]
+list_mute=[]
+i=0
+while i<12:
+    list_mute.append(False)
+    i+=1
+
+
+i=0
+j=0
+while j<4:
+    while i<64:
+        list_velos[j].append(0)
+        i+=1
+    i=0
+    j+=1
+
+
+def tri_velo(arguments):
+        #print("trivelo",arguments)
+        global lastIdInstru, lastIdPas
+        list_velos[lastIdInstru-1][lastIdPas]=arguments[0]
+        #print(list_velos[lastIdInstru])
+
+
+def tri_mute():
+    global lastIdInstru
+    if list_mute[lastIdInstru-1]==True:
+        list_mute[lastIdInstru-1]=False
+    elif list_mute[lastIdInstru-1]==False:
+        list_mute[lastIdInstru-1]=True
+
+def clear_velo():
+    global lastIdInstru
+    i=0
+    while i<64:
+        list_velos[lastIdInstru-1][i]=0
+        i+=1
+
+
+
+def default_handler(address, *args):
+    #print(address,args)
+    global lastIdInstru, lastIdPas, lastPas, list_velos,lastIdMenu,list_mute
+    if(address=="/idMenu"):
+        lastIdMenu=round(args[0])
+    if(address=="/idInstru"):
+        lastIdInstru=round(args[0])
+    if(address=="/idPas"):
+        lastIdPas=round(args[0])
+        #print("idPas:",lastIdPas)
+    if(address=="/pas"):
+        lastPas=round(args[0])
+    if(address=="/velo"):
+        tri_velo(args)
+    if(address=="/muteId"):
+        tri_mute()
+    if(address=="/clear"):
+        clear_velo()
+    if lastIdMenu==1:
+        affSeq(lastIdInstru,lastIdPas,lastPas,list_velos[lastIdInstru-1])
+    if lastIdMenu==0:
+        affMenu(lastIdInstru,list_mute)
+
+
+dispatcher = Dispatcher()
+dispatcher.set_default_handler(default_handler)
+
+ip = "127.0.0.1"
+port = 1337
+
+server = BlockingOSCUDPServer((ip, port), dispatcher)
+server.serve_forever()  # Blocks forever
+
