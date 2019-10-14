491f4d7e37fc34f30dc126efd5ff1063b3341a2b
totolabricot <thomas-barbe@hotmail.com>
2019-10-14 21:18:52 +0200

    python-osc-test4

--- python/analyse.py
+++ python/analyse.py
@@ -1,3 +1,9 @@
+from pythonosc import osc_message_builder
+from pythonosc import udp_client
 
 def anal():
-    print("anal is nice",arguments)
+    client = udp_client.UDPClient('localhost', 1338)
+    msg = osc_message_builder.OscMessageBuilder(address="/debug")
+    msg = msg.build()
+    print('Sending')
+    client.send(msg)
